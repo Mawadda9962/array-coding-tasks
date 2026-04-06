ffd7697f24c839543ad3f314a55d6c5c682e0cfe
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-06 17:04:08 +0400

    Solving task18

---
+++ src/Task18.java
@@ -0,0 +1,12 @@
+public class Task18 {
+    public static void main(String[] args) {
+        int[] pi =makePi();
+        for(Integer n:pi){
+            System.out.println(n);
+        }
+    }
+    public static int[]  makePi(){
+        return new int[]{3, 1, 4};
+    }
+}
+
